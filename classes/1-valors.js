4cd3ff71b09495e668d42bce5d260b0fcd6753eb
Adonys Santos <adonysmundotech@gmail.com>
2021-05-23 18:31:06 -0400

    Create valors.js

---
+++ classes/1-valors.js
@@ -0,0 +1,13 @@
+// TIPO PRIMITIVO
+40; // Number
+("Adonys Santos"); // String
+true; // Boolean 1
+false; // Boolean 0
+null; // Null
+undefined[ // Undefined
+  // TIPO OBJETO
+  (1, 2, 3)
+]; // Array
+{
+  name: "Adonys";
+} // Object
