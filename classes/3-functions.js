1e121d164ba460e635ed9358b4738a46a85ed0f9
Adonys Santos <adonysmundotech@gmail.com>
2021-05-23 19:21:50 -0400

    Create functions.js

---
+++ classes/3-functions.js
@@ -0,0 +1,14 @@
+// Tipo Declarativa
+function name(params) {
+  //code..
+}
+
+// Tipo Expresión
+var name = function (params) {
+  //code..
+};
+
+// Funciones flecha
+const name = () => {
+  //code..
+};
