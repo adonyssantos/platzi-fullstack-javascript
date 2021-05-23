8ac184956fdaa0b8d5b5f0f699f9d6b1d51c2d7b
Adonys Santos <adonysmundotech@gmail.com>
2021-05-23 18:59:44 -0400

    Create variables.js

---
+++ classes/2-variables.js
@@ -0,0 +1,10 @@
+// Variable Global
+var name = "Adonys";
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var users = { full_name: "Adonys", birthday: 0 };
+
+// Variable local
+let age = 17;
+
+// Constante
+const independenceDay = "February 27th";
